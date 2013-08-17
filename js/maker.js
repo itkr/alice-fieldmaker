577896e3c3dfe98cb2476eae49236462531ca6f5
itkr <g600392@gmail.com>
2013-08-17 23:44:42 +0900

    Update maker.js
    リファクタ、部分更新

--- js/maker.js
+++ js/maker.js
@@ -44,17 +44,23 @@
 			},
 
 			SelectorButton : function(element, type) {
-				var type = 0;
+				var type = type;
 				var selected = false;
+
+				this.element = element;
 				this.is_selected = function() {
 					return selected;
 				};
 				this.select = function() {
 					selected = true;
+					element.style.border = '3px solid #ff0000';
+					//仮
 					return this;
 				};
 				this.unselect = function() {
 					selected = false;
+					element.style.border = '3px solid #ffffff';
+					//仮
 					return this;
 				};
 				this.getType = function() {
@@ -64,18 +70,40 @@
 
 			SelectorField : function(element) {
 				var selectorButtons = [];
+				var selectingButtonType = 0;
 
 				this.appendSelectorButtons = function(elem, type) {
-					selectorButtons.push(new self.SelectorButton(elem, type));
+					var button = new self.models.SelectorButton(elem, type);
+					selectorButtons.push(button);
+					return button;
+					// 仮
 				};
 
+				this.getSelectorButtons = function() {
+					return selectorButtons;
+				};
+
+				this.setSelecting = function(type) {
+					var i;
+					selectingButtonType = type;
+					for ( i = 0; i < selectorButtons.length; i++) {
+						selectorButtons[i].unselect();
+					}
+					selectorButtons[type].select();
+				};
+
+				this.getSelecting = function() {
+					return selectingButtonType;
+				};
 			}
 		};
 		return objects;
 	})();
 
 	self.API = (function() {
-		var objects = {};
+		var objects = {
+			models : self.models
+		};
 		return objects;
 	})();
 
@@ -93,6 +121,7 @@
 	var PANEL_SIZE = 48;
 	var PANEL_LENGTH = 10;
 	var currentPanelId = 0;
+	var selectorField;
 	var fieldList = [];
 	var imageFileNames = ['panel_wall.png', 'panel_top_bottom.png', 'panel_left_right.png', 'panel_top_left.png', 'panel_top_right.png', 'panel_bottom_left.png', 'panel_bottom_right.png', 'panel_flat.png'];
 
@@ -111,8 +140,11 @@
 	 */
 	function initField(element, panel_len, panel_size) {
 		var newTable = document.createElement('table');
-		var newTr, newTd, newText;
-		for (var i = 0; i < panel_len * panel_len; i++) {
+		var newTr;
+		var newTd;
+		var newText;
+		var i = 0;
+		for ( i = 0; i < panel_len * panel_len; i++) {
 			if (i % panel_len === 0) {
 				newTr = document.createElement('tr');
 				newTable.appendChild(newTr);
@@ -124,6 +156,8 @@
 			newTd.style.backgroundColor = '#cccccc';
 			newTd.style.backgroundImage = 'url(' + DIR + PANELS + ')';
 			newTd.style.backgroundSize = panel_size * imageFileNames.length + 'px ' + panel_size + 'px';
+			
+			//初期表示の変化を付けてる
 			if (i % panel_len === 0 || i < panel_len || i > panel_len * panel_len - panel_len || i % panel_len == panel_len - 1) {
 				newTd.style.backgroundPosition = 0 * panel_size + 'px' + ' 0';
 				currentPanelId = 0;
@@ -131,8 +165,10 @@
 				newTd.style.backgroundPosition = 1 * panel_size + 'px' + ' 0';
 				currentPanelId = 7;
 			}
+			
 			newTd.appendChild(newText);
 			newTr.appendChild(newTd);
+
 			(function(_i) {
 				newTd.onclick = function() {
 					this.style.backgroundPosition = -(currentPanelId * PANEL_SIZE).toString() + 'px' + ' 0';
@@ -141,6 +177,7 @@
 					$('output').innerHTML = fieldList;
 				};
 			})(i);
+
 			fieldList[i] = currentPanelId;
 		}
 		newTable.style.backgroundColor = '#000000';
@@ -153,26 +190,21 @@
 	 * @param : element 表示先element
 	 */
 	function initSelectors(element) {
-		var images = element.getElementsByTagName('img');
+		var selectorButton;
+		selectorField = new FieldMaker.models.SelectorField(element);
 		for (var i = 0; i < imageFileNames.length; i++) {
+			var newImg = document.createElement('img');
+			selectorButton = selectorField.appendSelectorButtons(newImg, i);
+			newImg.setAttribute('src', DIR + imageFileNames[selectorButton.getType()]);
+			element.appendChild(selectorButton.element);
 			(function(_i) {
-				var newA = document.createElement('a');
-				var newImg = document.createElement('img');
-				newImg.setAttribute('src', DIR + imageFileNames[_i]);
-				newImg.style.border = '3px solid #ffffff';
-				newA.setAttribute('href', 'javascript:void(0)');
-				newA.appendChild(newImg);
-				element.appendChild(newA);
-				newA.onclick = function() {
-					for (var x = 0; x < images.length; x++) {
-						images[x].style.border = '3px solid #ffffff';
-					}
-					images[_i].style.border = '3px solid #ff0000';
+				selectorButton.element.onclick = function() {
+					selectorField.setSelecting(_i);
 					currentPanelId = _i;
 				};
 			})(i);
 		}
-		images[0].style.border = '3px solid #ff0000';
+		selectorField.setSelecting(0);
 	}
 
 })(this.document);
